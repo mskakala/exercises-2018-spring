b24e333c33b09e9eb2c1c2bbd26ed301abdd4331
Martin Podloucký <podlouckymartin@gmail.com>
2018-06-25 13:51:49 +0200

    New version of the React Clock.

--- day46-react-introduction/clock-react/src/clock/clock.js
+++ day46-react-introduction/clock-react/src/clock/clock.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import './clock.css';
 
+class DayPeriod extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      period: props.period
+    };
+  }
+
+  flip() {
+    let period = 'am';
+    if(this.state.period == 'am') {
+      period = 'pm';
+    }
+    
+    this.setState({
+      period: period
+    });
+  }
+
+  render() {
+    return (
+      <span className={this.state.period}
+            onClick={this.flip.bind(this)}>
+        {this.state.period}
+      </span> 
+    );
+  }
+}
+
 export default class Clock extends React.Component {
   constructor(props) {
     super(props);
@@ -12,7 +41,7 @@ export default class Clock extends React.Component {
   
   increment_hours() {
     this.setState({
-      hours: (this.state.hours + 1) % 25,
+      hours: (this.state.hours + 1) % 12,
       minutes: this.state.minutes
     });
   }
@@ -31,6 +60,8 @@ export default class Clock extends React.Component {
               onClick={this.increment_hours.bind(this)}>{this.state.hours}</span>:
         <span className="minutes"
               onClick={this.increment_minutes.bind(this)}>{this.state.minutes}</span>
+        &nbsp;
+        <DayPeriod period="am" />
       </div>
     );
   }
